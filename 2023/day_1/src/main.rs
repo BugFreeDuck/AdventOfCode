2be7bbb7b544acf855257471196c03e0eb182881
BugFreeDuck <bugfreeduck@gmail.com>
2023-12-01 20:09:11 +0200

    Fixing warnings, notes to self

--- 2023/day_1/src/main.rs
+++ 2023/day_1/src/main.rs
@@ -1,6 +1,17 @@
+#![allow(dead_code)]
+
 use std::{fs::File, env, io::{BufReader, BufRead}};
 
 static LEGAL_WORDS: &'static [&str] = &["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+/*
+    Quite a lot of clutter
+    Getting back to rust ways of doing things, 
+    This time just went with general "Feeling" without focusing too much on correctness/cleanliness of the code
+    To get answers for different parts, replace .map on line 20 with:
+        first_digit()          for first part answer
+        first_digit_or_word()  for second part answer
+*/
 
 fn main() {
     let input = read_input();
@@ -15,7 +26,7 @@ fn main() {
 
 fn first_digit(chars: impl Iterator<Item = char>) -> u32 {
     for char in chars{
-        if(char.is_ascii_digit()){
+        if char.is_ascii_digit() {
             return char.to_digit(10).unwrap();
         }
     }
@@ -26,7 +37,7 @@ fn first_digit(chars: impl Iterator<Item = char>) -> u32 {
 fn first_digit_or_word(chars: impl Iterator<Item = char>) -> u32 {    
     let mut word_buff = Vec::new();
     for char in chars{
-        if(char.is_ascii_digit()){
+        if char.is_ascii_digit() {
             return char.to_digit(10).unwrap();
         }
 
@@ -44,12 +55,12 @@ fn first_digit_or_word(chars: impl Iterator<Item = char>) -> u32 {
 
 fn parse_legal_word(test_word: &str) -> Option<u32>{
     for (value, word) in LEGAL_WORDS.iter().enumerate(){
-        if(test_word.contains(word)){
+        if test_word.contains(word) {
             return Some((value + 1) as u32)
         }
 
         let rev_test_word = word.chars().rev().collect::<String>();
-        if(test_word.contains(&rev_test_word)){
+        if test_word.contains(&rev_test_word) {
             return Some((value + 1) as u32) 
         }
     }
